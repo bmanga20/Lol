185df4d558066e39d5c514bdc84032454eed8aa7
Eric <ecrockett0@gmail.com>
2017-03-06 13:27:56 -0500

    Updated stack.yaml and cabal file

--- alchemy/Crypto/Alchemy/Interpreter/DepthEvaluator.hs
+++ alchemy/Crypto/Alchemy/Interpreter/DepthEvaluator.hs
@@ -4,16 +4,13 @@
 
 module Crypto.Alchemy.Interpreter.DepthEvaluator where
 
-import Algebra.Additive as Additive (C)
-import Algebra.Ring as Ring (C)
-
 import Crypto.Alchemy.Lam
 import Crypto.Alchemy.PTLang
 import Crypto.Lol
 
 -- | Metacircular evaluator with depth.
 newtype ID d a = ID { unID :: a }
-  deriving (Eq, Show, Additive.C, Ring.C)
+  deriving (Eq, Show)
 
 -- | Metacircular lambda with depth.
 instance LambdaD ID where
