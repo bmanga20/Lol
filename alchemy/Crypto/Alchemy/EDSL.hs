a19a6f431cf8957ea2a48d3e6e71a7bba04072c5
Eric <ecrockett0@gmail.com>
2017-03-09 20:35:44 -0500

    Typos and cleanup

--- alchemy/Crypto/Alchemy/EDSL.hs
+++ alchemy/Crypto/Alchemy/EDSL.hs
@@ -1,33 +1,10 @@
-{-# LANGUAGE ConstraintKinds            #-}
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures             #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE NoImplicitPrelude          #-}
-{-# LANGUAGE PolyKinds                  #-}
-{-# LANGUAGE RankNTypes                 #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances       #-}
 
 module Crypto.Alchemy.EDSL where
 
-import Crypto.Alchemy.Langauge.CT ()
+import Crypto.Alchemy.Language.CT ()
 import Crypto.Alchemy.Language.IR ()
-import Crypto.Alchemy.Langauge.Lam ()
-import Crypto.Alchemy.Langauge.PT ()
+import Crypto.Alchemy.Language.Lam ()
+import Crypto.Alchemy.Language.PT ()
 import Crypto.Alchemy.Interpreter.Evaluator ()
 import Crypto.Alchemy.Interpreter.DepthEvaluator ()
 import Crypto.Alchemy.Interpreter.PT2IR ()
-
-{- CJP: vestigial from we had entailAdditiveSymPT
-instance (SymCT ctexpr, Eq zp)
-  => Additive.C (PT2CT ctexpr (d :: Nat) (Cyc t m zp)) where
-
-  negate (P2CTerm a) = P2CTerm (\p zqs -> negate (a p zqs)
-                                 \\ witness entailRingSymCT (a p zqs))
--}
-
