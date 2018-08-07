01785d610f1103c8e0cd68d97e4e26687a7f83e1
bmanga <bmanga@umich.edu>
2018-08-07 10:35:09 -0400

    Update trapdoor functions

--- lol-apps/Crypto/Lol/Applications/Trapdoor.hs
+++ lol-apps/Crypto/Lol/Applications/Trapdoor.hs
@@ -11,75 +11,93 @@ Portability : POSIX
 Efficient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert/pubs/efftrap.pdf [MP12]>.
 -}
 
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+
 module Crypto.Lol.Applications.TrapdoorGeneration where
 
 import Control.Monad.Random
 
 import Crypto.Lol
 
-import MathObj.Matrix as Mtx
+import MathObj.Matrix as M
 
 -- TYPES --
 
-data MatrixStatic (r :: SPos) (c :: SPos) a =
-  MS :: r -> c -> Matrix a
-
-data MatrixStatic (SPos r) (SPos c) a =
-  something...
-
-type family
-mtxStatic :: r -> c -> a -> Matrix a
-
-data PublicKey n gad rq = PK { aBar :: PublicParam n rq
-                             , a'   :: Matrix rq  -- also a PublicParam?
-                             , h    :: Tag n rq }
-
-newtype PublicParam n rq = ABar (Matrix rq)
-
-newtype Tag n rq = Tag (Matrix rq)
-
-data Trapdoor gad rq = TD (Matrix rq)
-
-data LWESecret = LWESec (Matrix rq)
-
-data LWEError =
-
-data LWEOutput = LWEOutput { sec :: LWESecret, err :: LWEError }
-
+data    PublicKey gad ff rq = PK    { aBar    :: PublicParam rq
+                                    , a'      :: Matrix rq
+                                    , h       :: Tag ff }
+newtype PublicParam rq      = Param { unParam :: Matrix rq }
+newtype Tag ff              = Tag   { unTag   :: ff }
+newtype Trapdoor gad rq     = Trap  { unTrap  :: Matrix rq }
+data    LWEOutput gad rq    = Out   { bBar    :: Matrix rq
+                                    , b'      :: Matrix rq }
+newtype LWESecret rq        = Sec   { unSec   :: rq }
+data    LWEError rq         = Err   { eBar    :: Matrix rq
+                                    , e'      :: Matrix rq }
 
 -- METHODS --
 
-gadgetMtx :: GadgetMtx n gad
-gadgetMtx
+genTrap :: forall ff gad rq r rnd .
+  (r ~ LiftOf rq, Reduce r rq,
+   Ring rq, Module ff rq, Gadget gad rq, RoundedGaussianCyc r,
+   MonadRandom rnd)
+  => PublicParam rq
+  -> Tag ff
+  -> rnd (Trapdoor gad rq, PublicKey gad ff rq)
+genTrap aBar t = do
+  let mBar = numColumns aBar
+      tGad = (unTag t *>) <$> untag $ gadget @gad
+  r :: Matrix r <- gaussianMtx mBar $ length tGad
+  return (Trap r, PK aBar (tGad - aBar * reduce <$> r) t)
 
-genTrap :: (MonadRandom rnd)
-  => Maybe (PublicParam n rq)
-  -> Maybe (Tag n rq)
-  -> rnd (Trapdoor gad rq, PublicKey gad rq)
-genTrap p t =
-  let rowDim = sPosToInt $ sing :: Sing n
-      aBar = fromMaybe (randomMtx rowDim $ numCols h) p
-      tagH = fromMaybe (identityMtx rowDim) h
-  in PK aBar 
+lweSecret :: forall ff gad rq .
+  (Field ff, Ring rq, Correct gad rq, Module ff rq)
+  => Trapdoor gad rq -> Tag ff -> LWEOutput gad rq -> LWESecret rq
+lweSecret r (Tag h) (Out bBar b') =
+  let s' = fst $ correct $ tag (bBar * r + b') @gad
+  in recip h *> s'
 
+lweError :: PublicKey gad ff rq -> LWEOutput gad rq -> LWESecret rq -> LWEError rq
+lweError (PK aBar a' _) (Out bBar b') s = Err (bBar - s * aBar) (b' - s * a')
 
-lweSecret :: Trapdoor gad -> Tag -> LWEOutput -> LWESecret
-lweSecret r h b =
+lweInv :: PublicKey gad ff rq -> Trapdoor gad rq -> Tag ff -> LWEOutput gad rq
+  -> (LWESecret rq, LWEError rq)
+lweInv a r h b = let s = lweSecret r h b in (s, lweError a b s)
 
-lweInv :: PublicKey gad -> Trapdoor gad -> Tag -> LWEOutput
-  -> (LWESecret, LWEError)
-lweInv a r h b =
-  let s = lweSecret r h b
-      e =
-  in (s,e)
+lwe :: forall gad ff rq . (_)
+  => Proxy gad
+  -> PublicKey gad ff rq -> Tag ff -> LWESecret rq -> LWEError rq
+  -> LWEOutput gad rq
+lwe pg (PK aBar a' _) h' s (Err eBar e') =
+  let tagShift = (unTag h' *>) <$> untag $ gadget @gad
+  in LWEOut (eBar + s*aBar) (e' + s*a' + tagShift)
 
-lwe :: PublicKey -> Tag -> LWESecret -> LWEError -> LWEOutput
-lwe a h' s e =
+lweRand :: forall gad ff rq rnd . (MonadRandom rnd, Random rq)
+  => Proxy gad
+  -> PublicKey gad ff rq -> Tag ff -> LWESecret rq
+  -> rnd (LWEOutput gad rq)
+lweRand a h' s = do
+  e <- rndError a
+  return lwe a h' s e
 
-lweRand :: PublicKey -> Tag -> LWESecret -> LWEOutput
-lweRand a h' s =
-  let e = -- generate e of appropriate dimension ( = # cols in A )
-  in lwe a h' s e
+rndSecret :: (MonadRandom rnd, Random rq, RoundedGaussianCyc rq)
+  => rnd (LWESecret rq)
+rndSecret = roundedGaussian var
 
--- f in forward direction
-syndrome ::
+rndError :: (MonadRandom rnd, Random rq, RoundedGaussianCyc rq)
+  => PublicKey gad ff rq -> rnd (LWEError rq)
+rndError pk = let dim1 = numColumns $ aBar pk
+                  dim2 = numColumns $ a'   pk
+              in Err (gaussianMtx 1 dim1) (gaussianMtx 1 dim2)
+
+-- SUBROUTINES --
+
+gaussianMtx :: (MonadRandom rnd, RoundedGaussianCyc a)
+  => Int -> Int -> rnd (Matrix a)
+gaussianMtx r c = M.fromList r c <$> replicateM (r*c) $ roundedGaussian var
+
+-- BM: fixed value for variance OR pass as argument into functions?
+var :: Double
+var = 1.0
