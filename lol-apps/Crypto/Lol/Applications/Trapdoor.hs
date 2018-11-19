c07364d7b2d6001f281923ae6851346e752b7923
bmanga <bmanga@umich.edu>
2018-11-18 22:01:48 -0500

    Update naming for clarity

--- lol-apps/Crypto/Lol/Applications/Trapdoor.hs
+++ lol-apps/Crypto/Lol/Applications/Trapdoor.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : Crypto.Lol.Applications.Trapdoor
-Description : Etagicient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert/pubs/etagtrap.pdf [MP12]>.
+Description : Efficient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert/pubs/efftrap.pdf [MP12]>.
 Copyright   : (c) Bogdan Manga, 2018
                   Chris Peikert, 2018
 License     : GPL-3
@@ -8,9 +8,10 @@ Maintainer  : cpeikert@alum.mit.edu
 Stability   : experimental
 Portability : POSIX
 
-Etagicient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert/pubs/etagtrap.pdf [MP12]>.
+Efficient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert/pubs/efftrap.pdf [MP12]>.
 -}
 
+{-# LANGUAGE ConstraintKinds       #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE NoImplicitPrelude     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
@@ -19,8 +20,8 @@ Etagicient lattice trapdoor operations from <http://web.eecs.umich.edu/~cpeikert
 
 module Crypto.Lol.Applications.Trapdoor
 ( PublicKey, PublicParam(..), Trapdoor, LWEOutput, LWESecret(..), LWEError(..)
-, genTrap, rndTag, rndPublicParam
-, lweInv, lweRand, rndSecret
+, TrapGenCtx, trapGen, trapTag, publicParam
+, LWECtx, lweInv, lweRandomError, lweRandom, lweSecret
 ) where
 
 import Control.Applicative hiding ((*>))
@@ -43,109 +44,131 @@ newtype LWESecret rq         = Sec   { unSec   :: rq }
 data    LWEError rq          = Err   { eBar    :: Matrix rq
                                      , e'      :: Matrix rq }
 
+-- | Constraint synonym for generating a trapdoor.
+type TrapGenCtx gad tag cm zq
+  = (Reduce (cm (LiftOf zq)) (cm zq),
+    Ring (cm zq), Module tag (cm zq), Gadget gad (cm zq),
+    RoundedGaussianCyc cm (LiftOf zq))
+
+-- | Constraint synonym for computing the LWE function.
+type LWECtx gad tag rq r
+  = (Gadget gad rq, Reduce r rq, Module tag rq)
+
 -- METHODS --
 
-genTrap :: forall gad tag cm zq rnd .
-  (Reduce (cm (LiftOf zq)) (cm zq),
-   Ring (cm zq), Module tag (cm zq), Gadget gad (cm zq),
-   RoundedGaussianCyc cm (LiftOf zq), MonadRandom rnd)
+-- | Generate a trapdoor by sampling over a rounded Gaussian distribution.
+trapGen :: forall gad tag cm zq rnd .
+  (TrapGenCtx gad tag cm zq, MonadRandom rnd)
   => PublicParam (cm zq)
   -> tag
   -> rnd (Trapdoor gad (cm zq), PublicKey gad tag (cm zq))
-genTrap pp@(Param aBar) t = do
+trapGen pp@(Param aBar) t = do
   let mBar = numColumns aBar
-      tGad = singleRowMtx $ (t *>) <$> (gadget @gad)
+      tGad = rowMtx $ (t *>) <$> (gadget @gad)
   rBar :: Matrix (cm (LiftOf zq)) <- gaussianMtx mBar $ numColumns tGad
   r :: Matrix (cm (LiftOf zq)) <- gaussianMtx mBar $ numColumns tGad
   let (rBar', r') = (reduce <$> rBar, reduce <$> r)
   return (Trap rBar', PK pp (tGad - aBar * rBar' - r') t)
 
-lweSecret :: forall gad tag rq .
-  (Field tag, Ring rq, Correct gad rq, Module tag rq)
-  => Trapdoor gad rq -> tag -> LWEOutput gad rq
-  -> LWESecret rq
-lweSecret (Trap rBar) h (Out bBar b') =
-  let s' = fst $ correct @gad $ topRow (bBar * rBar + b')
-  in Sec $ recip h *> s'
+-- | Compute the LWE function.
+lwe :: forall gad tag rq r . (LWECtx gad tag rq r)
+  => PublicKey gad tag rq -> tag -> LWESecret rq -> LWEError r
+  -> LWEOutput gad rq
+lwe (PK (Param aBar) a' _) h' (Sec s) (Err eBar e') =
+  let tagShift = rowMtx $ (h' *>) <$> gadget @gad
+      bBar = (reduce <$> eBar) + scale s aBar
+      b'   = (reduce <$> e') + scale s (a' + tagShift)
+  in Out bBar b'
 
-lweError :: (LiftCyc cm zq, Ring (cm zq))
+-- | Compute the LWE function with randomly-generated error.
+lweRandomError :: forall gad tag cm zq rnd .
+  (Gadget gad (cm zq), Reduce (cm (LiftOf zq)) (cm zq), Module tag (cm zq),
+   MonadRandom rnd, RoundedGaussianCyc cm (LiftOf zq))
+  => PublicKey gad tag (cm zq) -> tag -> LWESecret (cm zq)
+  -> rnd (LWEOutput gad (cm zq))
+lweRandomError a h' s = do
+  e <- lweError a
+  return $ lwe a h' s e
+
+-- | Compute the LWE function with randomly-generated secret and error.
+lweRandom :: forall gad tag cm zq rnd .
+  (Gadget gad (cm zq), Reduce (cm (LiftOf zq)) (cm zq), Module tag (cm zq),
+   MonadRandom rnd, RoundedGaussianCyc cm zq, RoundedGaussianCyc cm (LiftOf zq))
+  => PublicKey gad tag (cm zq) -> tag
+  -> rnd (LWESecret (cm zq), LWEOutput gad (cm zq))
+lweRandom a h' = do
+  s <- lweSecret
+  lweOut <- lweRandomError a h' s
+  return $ (s, lweOut)
+
+-- | Generate an LWE secret by sampling from a rounded Gaussian distribution.
+lweSecret :: (MonadRandom rnd, RoundedGaussianCyc cm zq)
+  => rnd (LWESecret (cm zq))
+lweSecret = do
+  s <- roundedGaussian var
+  return $ Sec s
+
+-- | Generate an LWE error by sampling over a rounded Gaussian distribution.
+lweError :: (MonadRandom rnd, RoundedGaussianCyc cm (LiftOf zq))
+  => PublicKey gad tag (cm zq) -> rnd (LWEError (cm (LiftOf zq)))
+lweError (PK (Param aBar) a' _) = do
+  eBar <- gaussianMtx 1 $ numColumns aBar
+  e'   <- gaussianMtx 1 $ numColumns a'
+  return $ Err eBar e'
+
+-- | Recover LWE secret from LWE output by using trapdoor.
+lweInvSecret :: forall gad tag rq .
+  (Field tag, ZeroTestable tag, Ring rq, Correct gad rq, Module tag rq)
+  => Trapdoor gad rq -> tag -> LWEOutput gad rq
+  -> Maybe (LWESecret rq)
+lweInvSecret (Trap rBar) h (Out bBar b') =
+  let s' = fst $ correct @gad $ head $ rows (bBar * rBar + b')
+  in if isZero h then Nothing else Just $ Sec $ recip h *> s'
+
+-- | Recover LWE error from LWE output by using LWE secret.
+lweInvError :: (LiftCyc cm zq, Ring (cm zq))
   => PublicKey gad tag (cm zq) -> LWEOutput gad (cm zq) -> LWESecret (cm zq)
   -> LWEError (cm (LiftOf zq))
-lweError (PK (Param aBar) a' _) (Out bBar b') (Sec s) =
+lweInvError (PK (Param aBar) a' _) (Out bBar b') (Sec s) =
   let eBar = liftDec <$> (bBar - scale s aBar)
       e'   = liftDec <$> (b' - scale s a')
   in Err eBar e'
 
-lweInv :: (Field tag, Ring (cm zq), Correct gad (cm zq), Module tag (cm zq),
+-- | Recover LWE secret and LWE error by using trapdoor.
+lweInv :: (Field tag, ZeroTestable tag, Ring (cm zq),
+           Correct gad (cm zq), Module tag (cm zq),
            LiftCyc cm zq)
   => PublicKey gad tag (cm zq)
   -> Trapdoor gad (cm zq)
   -> tag
   -> LWEOutput gad (cm zq)
-  -> (LWESecret (cm zq), LWEError (cm (LiftOf zq)))
-lweInv a rBar h b = let s = lweSecret rBar h b in (s, lweError a b s)
+  -> Maybe (LWESecret (cm zq), LWEError (cm (LiftOf zq)))
+lweInv a rBar h b = do
+  s <- lweInvSecret rBar h b
+  return (s, lweInvError a b s)
 
-lwe :: forall gad tag rq r . (Gadget gad rq, Reduce r rq, Module tag rq)
-  => PublicKey gad tag rq -> tag -> LWESecret rq -> LWEError r
-  -> LWEOutput gad rq
-lwe (PK (Param aBar) a' _) h' (Sec s) (Err eBar e') =
-  let tagShift = singleRowMtx $ (h' *>) <$> gadget @gad
-      bBar = (reduce <$> eBar) + scale s aBar
-      b'   = (reduce <$> e') + scale s (a' + tagShift)
-  in Out bBar b'
+-- | Generate a uniformly-random tag.
+trapTag :: (MonadRandom rnd, Random tag) => rnd tag
+trapTag = getRandom
 
-lweRand :: forall gad tag cm zq rnd .
-  (Gadget gad (cm zq), Reduce (cm (LiftOf zq)) (cm zq), Module tag (cm zq),
-   MonadRandom rnd, RoundedGaussianCyc cm (LiftOf zq))
-  => PublicKey gad tag (cm zq) -> tag -> LWESecret (cm zq)
-  -> rnd (LWEOutput gad (cm zq))
-lweRand a h' s = do
-  e <- rndError a
-  return $ lwe a h' s e
-
-lweRand' :: forall gad tag cm zq rnd .
-  (Gadget gad (cm zq), Reduce (cm (LiftOf zq)) (cm zq), Module tag (cm zq),
-   MonadRandom rnd, RoundedGaussianCyc cm zq, RoundedGaussianCyc cm (LiftOf zq))
-  => PublicKey gad tag (cm zq) -> tag
-  -> rnd (LWESecret (cm zq), LWEOutput gad (cm zq))
-lweRand' a h' = do
-  s <- rndSecret
-  lweOut <- lweRand a h' s
-  return $ (s, lweOut)
-
-rndSecret :: (MonadRandom rnd, RoundedGaussianCyc cm zq)
-  => rnd (LWESecret (cm zq))
-rndSecret = do
-  s <- roundedGaussian var
-  return $ Sec s
-
-rndError :: (MonadRandom rnd, RoundedGaussianCyc cm (LiftOf zq))
-  => PublicKey gad tag (cm zq) -> rnd (LWEError (cm (LiftOf zq)))
-rndError (PK (Param aBar) a' _) = do
-  eBar <- gaussianMtx 1 $ numColumns aBar
-  e'   <- gaussianMtx 1 $ numColumns a'
-  return $ Err eBar e'
-
-rndTag :: (MonadRandom rnd, Random tag) => rnd tag
-rndTag = getRandom
-
-rndPublicParam :: (MonadRandom rnd, Random rq) => Int -> rnd (PublicParam rq)
-rndPublicParam mBar = do
+-- | Generate a uniformly-random public parameter.
+publicParam :: (MonadRandom rnd, Random rq) => Int -> rnd (PublicParam rq)
+publicParam mBar = do
   pp <- M.fromList 1 mBar <$> replicateM mBar getRandom
   return $ Param pp
 
 -- SUBROUTINES --
 
+-- | Generate random matrix with specified dimensions by sampling over a
+-- rounded Gaussian distribution.
 gaussianMtx :: (MonadRandom rnd, RoundedGaussianCyc cm a)
   => Int -> Int -> rnd (Matrix (cm a))
 gaussianMtx r c = M.fromList r c <$> replicateM (r*c) (roundedGaussian var)
 
--- BM: fixed value for variance OR pass as argument into functions?
+-- | Hard-coded value for variance of rounded Gaussian distribution.
 var :: Double
 var = 1.0
 
-topRow :: Matrix a -> [a]
-topRow = head . rows
-
-singleRowMtx :: [a] -> Matrix a
-singleRowMtx a = M.fromList 1 (length a) a
+-- | Create one-row matrix from input list.
+rowMtx :: [a] -> Matrix a
+rowMtx a = M.fromList 1 (length a) a
